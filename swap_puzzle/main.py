145f0180da6f3392150ea00778dd9e053a394ed6
leo <leo.donzil@ensae.fr>
2024-02-12 08:55:06 +0000

    test bfs implémenté

--- swap_puzzle/main.py
+++ swap_puzzle/main.py
@@ -6,58 +6,16 @@ from grid import Grid
 from solver import Solver
 from graph import Graph
 
-#Question 3 : naive solver
-solver=Solver()
+# Question 3 : naive solver
+solver = Solver()
 """grid = Grid(2, 3, [[5, 3, 6], [2, 1, 4]])
 solver.naive_solver(grid)"""
 
-#Question 4
+# Question 4
 """G = Grid(10,10)
 G.grid_representation()"""
 
-#Question 5 : we test all the lines
-def test_graph(graph_number):
-    """this function gets the graph in graph.in and tests if the bfs function works with the graph.path.out, by testing bfs(src,dst) (with src and dst the first 2 numbers of each line in graph1.in) and then compares it to the list at the end of the line"""
-    G = Graph.graph_from_file(f"/home/onyxia/work/ensae-prog24/input/graph"+str(graph_number)+".in")
-    # Open the file in read mode
-    with open(f"/home/onyxia/work/ensae-prog24/input/graph"+str(graph_number)+".path.out", "r") as file:
-        tuple_list = []  # list with tuples of integers, representing the first 2 numbers of each line in the file graph1.path.out
-        list_list = []  # list with lists of integers, representing the path in each line in the file graph1.path.out
-        
-        # Read each line of the file
-        for line in file:
-            elements = line.split(" ") #splits the elements in each line separed by a space
-            if elements[-1] == "None\n":
-                elements[-1] = "None"
-            else:
-                last_number = (elements.pop())[:-2]  # at the end of each line, is a "\n" in order to return to a new line in the file, so we extract the number
-                elements.append(last_number)
-                elements.append("]")  # puts back the "]"
-                num3 = eval(elements.pop(2))  # case where there is 3 numbers and 1 list
-            
-            # Convertir les trois premiers éléments en nombres entiers
-            num1 = eval(elements.pop(0))
-            num2 = eval(elements.pop(0))
-            
-            #removes the \n at the end of each line
-            list = "".join(elements)
-            path = eval(list) #python understands its a list, or a None object
-            tuple_nums = (num1, num2)
-            
-            tuple_list.append(tuple_nums)
-            list_list.append(path)
-
-    function_work = True
-    for i in range(len(list_list)):
-        path_bfs = G.bfs(tuple_list[i][0], tuple_list[i][-1])
-        if path_bfs != list_list[i]:
-            function_work = False
-
-    return "It works with graph number "+str(graph_number)+"!" if function_work else "It doesn't work"
-
-
-"""print(test_graph(1))
-print(test_graph(2))"""
+# Question 5 : we test all the lines, with tests/test_bfs.py, and it do work
 
 # Question 6 :The idea is this:
 # We replace the list of lists with a list with all the coefficients and we will assign a number which will be the
